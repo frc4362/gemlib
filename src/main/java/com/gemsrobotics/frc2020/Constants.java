cfcd5263c4704714811e85021d664a586227f098
ejmalzone <ejmalzone@gmail.com>
2020-02-08 14:53:01 -0500

    Update Constants.java

--- src/main/java/com/gemsrobotics/frc2020/Constants.java
+++ src/main/java/com/gemsrobotics/frc2020/Constants.java
@@ -3,6 +3,7 @@ package com.gemsrobotics.frc2020;
 public class Constants {
 	private Constants() { }
 
-	public static final int HOPPER_PORT = 11;
+	public static final int HOPPER_MOTOR_PORT = 7;
+	public static final int HOPPER_COLOR_SENSOR_PORT = 1;
 	public static final int TURRET_PORT = 39;
 }
